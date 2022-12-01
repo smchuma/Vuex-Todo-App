60f752aa493f6e02d011034e871beef86e6f2077
Samora Laurent <laurentsamora6@gmail.com>
2022-12-02 01:16:19 +0300

    implemented checkbox and remove todo

---
+++ src/components/List/ListItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="view" key="todo.id">
+    <input
+      type="checkbox"
+      :checked="todo.completed"
+      @change="handleCompleted"
+    />
+    <label>{{ todo.title }}</label>
+    <button class="remove">
+      <img src="../../assets/ICON-CROSS.SVG" alt="cross" @click="removeTodo" />
+    </button>
+  </div>
+  <input class="edit" />
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  setup(props, { emit }) {
+    const removeTodo = () => emit("remove", props.todo.id);
+    const handleCompleted = () =>
+      emit("update", { id: props.todo.id, completed: !props.todo.completed });
+
+    return {
+      removeTodo,
+      handleCompleted,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/TodoList/TodoList.vue
+++ src/components/TodoList/TodoList.vue
@@ -1,11 +1,42 @@
 <template>
   <div class="container">
     <ul class="todo-list">
-      <!-- <li>Hello there</li> -->
+      <li
+        v-for:="todo in visibleTodos"
+        :class="{ completed: todo.completed, editing: editing }"
+      >
+        <ListItem :todo="todo" @remove="removeTodo" @update="updateTodo" />
+      </li>
     </ul>
   </div>
 </template>
 
-<script setup></script>
+<script>
+import { computed } from "vue";
+import { useStore } from "vuex";
+import ListItem from "../List/ListItem.vue";
+export default {
+  name: "TodoList",
+  components: {
+    ListItem,
+  },
+  setup() {
+    const store = useStore();
 
-<style lang="scss" scoped></style>
+    const removeTodo = (todoId) => store.dispatch("removeTodo", todoId);
+    console.log("removeTodo", removeTodo);
+    const updateTodo = (todo) => store.dispatch("updateTodo", todo);
+    const completeAll = () => store.dispatch("completeAll");
+    return {
+      visibleTodos: computed(() => store.getters.visibleTodos),
+      areAllCompleted: computed(() => store.getters.areAllCompleted),
+      removeTodo,
+      updateTodo,
+      completeAll,
+    };
+  },
+};
+</script>
+<style lang="scss">
+@import "./TodoList.scss";
+</style>
